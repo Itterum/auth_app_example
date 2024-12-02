0fdc94ab66510047b7b20d950830b8e1ea7983e1
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2024-12-02 16:42:03 +0300

    impl save themes settings

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,8 +2,19 @@ import 'package:crud_flutter_example/app/router.dart';
 import 'package:crud_flutter_example/app/theme.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    AppThemeSettings.loadTheme();
+  }
 
   @override
   Widget build(BuildContext context) {
